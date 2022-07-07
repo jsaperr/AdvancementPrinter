694ed0df9e2b5a629bc5d8483d8b589c78236d44
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-07-07 19:00:15 +0000

    Update AdvancementPrinter.java

--- src/main/java/jsaper/advancementprinter/AdvancementPrinter.java
+++ src/main/java/jsaper/advancementprinter/AdvancementPrinter.java
@@ -1,5 +1,7 @@
 package jsaper.advancementprinter;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -8,16 +10,18 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 
 public class AdvancementPrinter implements ModInitializer {
-
     public static final  String MOD_ID = "advancementprinter";
-    public static final String MOD_NAME = "Advancement Printer";
+    public static final ModContainer MOD_CONTAINER = FabricLoader.getInstance().getModContainer(MOD_ID).get();
+    public static final String MOD_NAME = MOD_CONTAINER.getMetadata().getName();
     public static File FILE_DIR;
-    public static Logger LOGGER = LogManager.getLogger();
+    public static Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
     public static void log(Level level, String message) {
         LOGGER.log(level, "[" + MOD_NAME + "] " + message);
     }
+    
     @Override
     public void onInitialize() {
+        log("Initializing");
     }
 }
